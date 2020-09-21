870f6ee628c4dd92bb4bf07aef83e454621ea561
haeryung <haeryung96@naver.com>
2020-09-21 16:50:25 +0900

    TH-Sensor 실시간 그래프 추가

--- app/src/main/java/org/techtown/uiproject/cameralist/CameraListActivity.java
+++ app/src/main/java/org/techtown/uiproject/cameralist/CameraListActivity.java
@@ -1,20 +1,53 @@
 package org.techtown.uiproject.cameralist;
 
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 import org.techtown.uiproject.R;
+import org.techtown.uiproject.mypage.Register;
+import org.techtown.uiproject.mypage.mypageActivity;
+import org.techtown.uiproject.sensor.HumidityActivity;
+import org.techtown.uiproject.sensor.TemperatureActivity;
 
 
 public class CameraListActivity extends AppCompatActivity {
+
+    Button cameraBtn, humBtn, tempBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_camera_list);
 
+        cameraBtn = findViewById(R.id.cameraBtn);
+        humBtn = findViewById(R.id.humBtn);
+        tempBtn = findViewById(R.id.tempBtn);
+
+        //카메라 버튼 누를시 액티비티 추가하기
+
+        //습도 버튼 클릭시
+        humBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(CameraListActivity.this, HumidityActivity.class));
+            }
+        });
+
+        //온도 버튼 클릭시
+        tempBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(CameraListActivity.this, TemperatureActivity.class));
+            }
+        });
     }
+
+
 
 
 
---
+++ app/src/main/java/org/techtown/uiproject/sensor/TemperatureActivity.java
@@ -0,0 +1,64 @@
+package org.techtown.uiproject.sensor;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import org.techtown.uiproject.R;
+
+import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.LineData;
+import com.github.mikephil.charting.data.LineDataSet;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+
+public class TemperatureActivity extends AppCompatActivity {
+
+    private LineChart lineChart;
+    ArrayList<Entry> yData;
+    DatabaseReference mPostReference;
+    ValueEventListener valueEventListener;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_temperature);
+
+        lineChart = (LineChart) findViewById(R.id.chart);
+
+        mPostReference = FirebaseDatabase.getInstance().getReference("sensor");
+        mPostReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                yData = new ArrayList<>();
+                float i = 0;
+                for (DataSnapshot ds : dataSnapshot.getChildren()) {
+                    i=i+1;
+                    String SV = ds.child("temperature").getValue().toString();
+                    Float SensorValue = Float.parseFloat(SV);
+                    yData.add(new Entry(i,SensorValue));
+                }
+                final LineDataSet lineDataSet = new LineDataSet(yData,"Temperature");
+                //lineDataSet.setColor(R.color.colorPrimaryDark);
+                lineDataSet.setLineWidth(3);
+                lineDataSet.setCircleRadius(4);
+                LineData data = new LineData(lineDataSet);
+                lineChart.setData(data);
+                lineChart.notifyDataSetChanged();
+                lineChart.invalidate();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+    }
+}
